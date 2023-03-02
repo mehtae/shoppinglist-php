8e0a0691efa939afd261227565aafaf5e2bb997f
mehtae <mehtaloidy@gmail.com>
2023-03-02 13:09:54 +0200

    fixed the 500 internal error

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 function getItems() {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
     if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         return 0;
     }
@@ -11,12 +14,13 @@ function getItems() {
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         header('HTTP/1.1 200 OK');
         print json_encode($results);
-    
+        exit();
+        
         } catch (PDOException $pdoex) {
             header('HTTP/1.1 500 Internal Server Error');
             $error = array('error' => $pdoex->getMessage());
             print json_encode($error);
-    }
-   
+            return;
+    } 
 }
 ?>
